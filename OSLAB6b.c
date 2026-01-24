72f5d48c6819c49fcf5c8b98ad08d76534ec384e
Naveen A <2006naveen18@gmail.com>
2026-01-24 12:44:39 +0530

    Implement process size input for best fit allocation
    Added input for number of processes and their sizes.

--- OSLAB6b.c
+++ OSLAB6b.c
@@ -45,5 +45,15 @@ int main() {
         blocks[i].allocated = 0;
     }
 
-  
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int processSize;
+        printf("Enter size of process %d: ", i + 1);
+        scanf("%d", &processSize);
+        bestFit(blocks, m, processSize);
+    }
+
+    return 0;
 }
