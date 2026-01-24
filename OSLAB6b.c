2f58a613ec335be15f2ce1d91d29c52106e5e794
Naveen A <2006naveen18@gmail.com>
2026-01-24 12:42:51 +0530

    Implement best-fit memory allocation in C

---
+++ OSLAB6b.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAX_BLOCKS 100
+
+// Structure to represent memory blocks
+struct MemoryBlock {
+    int block_id;
+    int size;
+    int allocated;
+};
+
+// Function to perform best-fit memory allocation
+void bestFit(struct MemoryBlock blocks[], int m, int processSize) {
+    int bestIdx = -1;
+
+    for (int i = 0; i < m; i++) {
+        if (blocks[i].allocated == 0 && blocks[i].size >= processSize) {
+            if (bestIdx == -1 || blocks[i].size < blocks[bestIdx].size) {
+                bestIdx = i;
+            }
+        }
+    }
+
+    if (bestIdx != -1) {
+        blocks[bestIdx].allocated = 1;
+        printf("Process of size %d allocated to block %d\n",
+               processSize, bestIdx + 1);
+    } else {
+        printf("Cannot allocate process of size %d\n", processSize);
+    }
+}
+
+int main() {
+    int m, n;
+
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &m);
+
+    struct MemoryBlock blocks[MAX_BLOCKS];
+
+    for (int i = 0; i < m; i++) {
+        blocks[i].block_id = i + 1;
+        printf("Enter size of memory block %d: ", i + 1);
+        scanf("%d", &blocks[i].size);
+        blocks[i].allocated = 0;
+    }
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int processSize;
+        printf("Enter size of process %d: ", i + 1);
+        scanf("%d", &processSize);
+        bestFit(blocks, m, processSize);
+    }
+
+    return 0;
+}
