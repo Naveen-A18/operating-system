a7bb1e3827b9c122a13d75d13a36848e79d2dbd5
Naveen A <2006naveen18@gmail.com>
2026-01-25 12:56:45 +0530

    Update OSLAB6c.c

--- OSLAB6c.c
+++ OSLAB6c.c
@@ -1,1 +1,54 @@
- 
+ #include <stdio.h>
+
+#define MAX_BLOCKS 100
+
+// Structure to represent memory blocks
+struct MemoryBlock {
+    int block_id;
+    int size;
+    int allocated;
+};
+
+// Function to perform first-fit memory allocation
+void firstFit(struct MemoryBlock blocks[], int m, int processSize) {
+    for (int i = 0; i < m; i++) {
+        if (blocks[i].allocated == 0 && blocks[i].size >= processSize) {
+            // Allocate the memory block
+            blocks[i].allocated = 1;
+            printf("Process of size %d allocated to block %d\n",
+                   processSize, i + 1);
+            return;
+        }
+    }
+    printf("Cannot allocate process of size %d\n", processSize);
+}
+
+int main() {
+    int m, n;
+
+    // Number of memory blocks
+    printf("Enter the number of memory blocks: ");
+    scanf("%d", &m);
+
+    struct MemoryBlock blocks[MAX_BLOCKS];
+
+    for (int i = 0; i < m; i++) {
+        blocks[i].block_id = i + 1;
+        printf("Enter the size of memory block %d: ", i + 1);
+        scanf("%d", &blocks[i].size);
+        blocks[i].allocated = 0; // Initialize as unallocated
+    }
+
+    // Number of processes
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int processSize;
+        printf("Enter the size of process %d: ", i + 1);
+        scanf("%d", &processSize);
+        firstFit(blocks, m, processSize);
+    }
+
+    
+}
