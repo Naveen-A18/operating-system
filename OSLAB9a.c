78bdb5429bc2a9c0c376924491e880c33e3787e6
Naveen A <2006naveen18@gmail.com>
2026-02-02 11:04:21 +0530

    Implement memory block allocation in OSLAB9a.c
    Added main function to manage memory blocks and user input.

--- OSLAB9a.c
+++ OSLAB9a.c
@@ -1,3 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
- 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int f[50], i, st, len, j, c, k, a;
+
+    // Initialize all blocks as free (0)
+    for (i = 0; i < 50; i++)
+        f[i] = 0;
+
+    // Already allocated blocks
+    printf("Enter how many blocks already allocated: ");
+    scanf("%d", &p);
+
+    printf("Enter blocks already allocated: ");
+    for (i = 0; i < p; i++) {
+        scanf("%d", &a);
+        f[a] = 1;
+    }
