237cd90f172a0c7f1c9e2602e53b228d7b9dcded
Naveen A <2006naveen18@gmail.com>
2026-02-02 11:04:41 +0530

    Implement block allocation and user interaction
    Added functionality to allocate blocks and handle user input for allocation.

--- OSLAB9a.c
+++ OSLAB9a.c
@@ -17,3 +17,39 @@ int main() {
         scanf("%d", &a);
         f[a] = 1;
     }
+
+x:
+    printf("Enter index starting block and length: ");
+    scanf("%d %d", &st, &len);
+
+    k = len;
+
+    if (f[st] == 0) {
+
+        for (j = st; j < (st + k); j++) {
+
+            if (f[j] == 0) {
+                f[j] = 1;
+                printf("%d --> ", j);
+            }
+            else {
+                printf("\n%d Block is already allocated\n", j);
+                k++;   // Move to next free block
+            }
+        }
+        printf("NULL\n");
+    }
+    else {
+        printf("%d starting block is already allocated\n", st);
+    }
+
+    printf("Do you want to enter more file (Yes - 1 / No - 0): ");
+    scanf("%d", &c);
+
+    if (c == 1)
+        goto x;
+    else
+        exit(0);
+
+    return 0;
+}
