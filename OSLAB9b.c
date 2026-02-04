27ab3045f0ec00de67a6f247f471245ff28ae1a8
Naveen A <2006naveen18@gmail.com>
2026-02-04 18:48:17 +0530

    Update OSLAB9b.c

--- OSLAB9b.c
+++ OSLAB9b.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+
 int main()
 {
-}
+    int n, head, max;
+    int queue[20], queue1[20], queue2[20];
+    int temp, temp1 = 0, temp2 = 0;
+    int i, j;
+    int seek = 0, diff;
+    float avg;
+
+    printf("Enter maximum disk range: ");
+    scanf("%d", &max);
+
+    printf("Enter initial head position: ");
+    scanf("%d", &head);
+
+    printf("Enter number of disk requests: ");
+    scanf("%d", &n);
+
+    printf("Enter disk requests:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &queue[i]);
